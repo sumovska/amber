36cd9e30b9cf345ace621d5fefc00b258cf199e6
Nataliia <sumovskaya@gmail.com>
2016-03-21 19:00:12 +0700

    #service

--- src/js/main.js
+++ src/js/main.js
@@ -15,23 +15,70 @@ $(document).ready(function () {
 	});
 
 	$('.about').each(function(){
-		$('.list', this).slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			mobileFirst: true,
-			adaptiveHeight: true,
-			infinite: false,
-			dots: true,
-			customPaging: function (slider, i) {
-				return '<span class="dots" data-role="none"></span>';
-			},
-			arrows: false,
-			responsive: [
-				{
-					breakpoint: 750,
-					settings: 'unslick'
+		var _list = $('.list', this),
+			settings = {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				mobileFirst: true,
+				adaptiveHeight: true,
+				infinite: false,
+				dots: true,
+				customPaging: function (slider, i) {
+					return '<span class="dots" data-role="none"></span>';
+				},
+				arrows: false,
+				responsive: [
+					{
+						breakpoint: 750,
+						settings: 'unslick'
+					}
+				]
+			};
+		_list.slick(settings);
+		$(window).on('resize', function () {
+			if ($(window).width() >= 750) {
+				if (_list.hasClass('slick-initialized')) {
+					_list.slick('unslick');
 				}
-			]
+			} else {
+				if (!_list.hasClass('slick-initialized')) {
+					_list.slick(settings);
+				}
+			}
+		});
+	});
+
+	$('.service').each(function(){
+		var _list = $('.list', this),
+			settings = {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				mobileFirst: true,
+				adaptiveHeight: true,
+				infinite: false,
+				dots: true,
+				customPaging: function (slider, i) {
+					return '<span class="dots" data-role="none"></span>';
+				},
+				arrows: false,
+				responsive: [
+					{
+						breakpoint: 750,
+						settings: 'unslick'
+					}
+				]
+			};
+		_list.slick(settings);
+		$(window).on('resize', function () {
+			if ($(window).width() >= 750) {
+				if (_list.hasClass('slick-initialized')) {
+					_list.slick('unslick');
+				}
+			} else {
+				if (!_list.hasClass('slick-initialized')) {
+					_list.slick(settings);
+				}
+			}
 		});
 	});
 
